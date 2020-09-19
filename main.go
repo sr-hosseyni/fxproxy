e5d52da3da85b564607678cf0a434624d0ea88ab
sr_hosseyni <rassoul.hosseini@gmail.com>
2020-09-19 20:08:59 +0300

    complete path validate function

--- main.go
+++ main.go
@@ -1,21 +1,35 @@
 package main
 
-var allowedList []string
+import (
+    "fmt"
+    "regexp"
+    "strings"
+)
+
+const ID_FORMAT = "[a-z]+[0-9]+[a-z0-9]+"
+
+var allowedList = [...]*regexp.Regexp{
+    regexp.MustCompile(`^company$`),                                                    // /company/
+    regexp.MustCompile(strings.Replace(`^company/{id}$`, "{id}", ID_FORMAT, 1)),        // /company/{id}
+    regexp.MustCompile(strings.Replace(`^company/[a-z0-9]+$`, "{id}", ID_FORMAT, 1)),   // /company/{id}
+    regexp.MustCompile(`^company/account$`),                                            // /company/account
+    regexp.MustCompile(`^account$`),                                                    // /account
+    regexp.MustCompile(strings.Replace(`^account/{id}$`, "{id}", ID_FORMAT, 1)),        // /account/{id}
+    regexp.MustCompile(strings.Replace(`^{id}$`, "{id}", ID_FORMAT, 1)),                // /{id}
+    regexp.MustCompile(strings.Replace(`^account/{id}/user$`, "{id}", ID_FORMAT, 1)),   // /?account/{id}/user
+    regexp.MustCompile(`^tenant/account/blocked$`),                                     // /tenant/account/blocked
+}
 
 func main() {
-	allowedList = []string{
-		"/company/",
-		"/company/{id}",
-		"/company/account",
-		"/account",
-		"/account/{id}",
-		"/{id}",
-		"/account/{id}/user",
-		"/tenant/account/blocked",
-	}
+    fmt.Println("Running ...")
 }
 
 func ValidatePath(path string) bool {
-	// Some magic here...
-	return false
+    for _, allowedPath := range allowedList {
+        if allowedPath.MatchString(strings.Trim(strings.ToLower(path), "/")) {
+            return true
+        }
+    }
+
+    return false
 }
